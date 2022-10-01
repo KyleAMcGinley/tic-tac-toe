ae22ef9998239d53e03decc23e3b15857cd93c9f
Kyle McGinley <kyleamcginley@gmail.com>
2022-10-01 08:39:31 -0400

    Made factory function for player

--- myscript.js
+++ myscript.js
@@ -1,5 +1,11 @@
 //Need to make module for gameboard
 const gameboard = (() => {
-
+    
 })();
+
+//Need object for players
+const player = (name) => {
+    const getName = () => name;
+return {getName};
+};
 
