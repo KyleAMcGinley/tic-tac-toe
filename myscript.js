2dff64bc0de84427a57809862d6072e16692458e
Kyle McGinley <kyleamcginley@gmail.com>
2022-10-01 08:47:30 -0400

    Will write code to display gameboard on webpage

--- myscript.js
+++ myscript.js
@@ -1,11 +1,13 @@
 //Need to make module for gameboard
 const gameboard = (() => {
-    
+    gameBoardArray = [];
 })();
 
 //Need object for players
 const player = (name) => {
     const getName = () => name;
-return {getName};
+
+    return {getName};
 };
 
+const me = player('Kyle');
