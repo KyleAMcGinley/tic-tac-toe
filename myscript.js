82fb5bbace811459af35899238c296f4dd9cb899
Kyle McGinley <kyleamcginley@gmail.com>
2022-10-13 17:03:18 -0400

    Rearranged everything

--- myscript.js
+++ myscript.js
@@ -4,118 +4,60 @@
 when a box is clicked. Need to obtain the data value of the box
 when clicked and add it to the gameboard array. */
 const gameboard = (() => {
-    //Function for displaying gameboard array
-    const boxes = document.querySelectorAll('div.box');
 
     gameBoardArray = ['', '', '', '', '', '', '', '', '', ];
+
+    return {gameBoardArray};
+})();
+
+const displayingGameboard = (() => {
 
     const displayingGame = () => {
         for(let i = 0; i < boxes.length; i++){
         boxes[i].textContent = gameBoardArray[i];
         }
     }
-
-    return {displayingGame};
+    return {displayingGame}
 })();
-
-gameboard.displayingGame();
 
 
 //Need object for controlling gameflow
 const gameFlow = (() => {
-    //Need to get value of X and O values in the array
+    
+    return {};
+})();
+
+//Need object for players that includes player's name, X or O choice, and whether going 1st or 2nd
+const player = (name, XorO, playerTurn) => {
     const boxes = document.querySelectorAll('div.box');
 
-    const findingEmptyBoxes = (word) => {
-        return word === '';
-    }
+    const getName = () => name;
+    const getXorO = () => XorO;
+    const getPlayerTurn = () => playerTurn;
 
-    const filteredEmptyBoxes = () => gameBoardArray.filter(findingEmptyBoxes);
-
-
-
-    let sumTotal = 0;
-
-    const getValuesInArray = (value) =>{
-        if(value === 'X' || value === 'O'){
-            sumTotal += 1;
+    const displayingGame = () => {
+        for(let i = 0; i < boxes.length; i++){
+        boxes[i].textContent = gameBoardArray[i];
         }
-    }
-
-    const resetSum = () => sumTotal = 0;
-
-    const addingXandO = () => {
-        for(let i = 0; i < gameBoardArray.length; i++){
-            getValuesInArray(gameBoardArray[i]);
-        } return sumTotal;
-    }
+    };
 
     const assigningListener = (listener, event) => {
         for(let i = 0; i < boxes.length; i++){
             boxes[i].addEventListener(listener, event);
         }
-    }
+    };
 
-    const removingEventListener = () => {
-        const box = event.target;
-        box.removeEventListener('click', changingDisplayToO);
-        box.removeEventListener('click', changingDisplayToX);
-    }
-
-    const changingDisplayToX = () => {
+    const changingDisplay = () => {
         const box = event.target.getAttribute('data-value');
-        gameBoardArray.splice(box, 1, 'X');
+        gameBoardArray.splice(box, 1, XorO);
         displayingGame();
-    }
+    };
 
-    const changingDisplayToO = () => {
-        const box = event.target.getAttribute('data-value');
-        gameBoardArray.splice(box, 1, 'O');
-        displayingGame();
-    }
+    assigningListener('click', changingDisplay);
 
-    const displayingGame = () => {
-        for(let i = 0; i < boxes.length; i++){
-        boxes[i].textContent = gameBoardArray[i];
-        }
-    }
-
-    const alternatingTurns = () => {
-            if((sumTotal + 1) % 2 === 0){
-                console.log('hello');
-            }else{
-                console.log('there');
-            }
-        }
-
-    const playingGame = (sumTotal) => {
-        if((sumTotal + 1) % 2 === 0){
-            assigningListener('click', changingDisplayToX)
-            changingDisplayToX;
-            resetSum;
-            addingXandO;
-            removingEventListener;
-        } else{
-            assigningListener('click', changingDisplayToO)
-            changingDisplayToO;
-            resetSum;
-            addingXandO;
-            removingEventListener;
-        }
-    }
-
-    return {filteredEmptyBoxes, changingDisplayToO, removingEventListener, assigningListener, changingDisplayToX, displayingGame, alternatingTurns, playingGame};
-})();
-
-
-gameFlow.playingGame();
-
-//Need object for players
-const player = (name) => {
-    const getName = () => console.log(name);
-
-    return {getName};
+    return {getName, getXorO, getPlayerTurn};
 };
 
-const playerO = player('Kyle');
-const playerX = player('Other');
+const firstPlayer = player('Other', 'O', 2);
+const secondPlayer = player('Kyle', 'X', 1);
+console.log(firstPlayer.getPlayerTurn())
